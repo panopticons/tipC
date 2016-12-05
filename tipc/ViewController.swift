9e69a335ab63f401c76a62e26cbf1fc792cc2c86
fer <fer@Stephanies-MacBook-Pro-2.local>
2016-12-05 04:08:55 -0700

    Initial Commit

---
+++ tipc/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tipc
+//
+//  Created by fer on 12/5/16.
+//  Copyright © 2016 fer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
